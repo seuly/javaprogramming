9a0d7ce2efef038806ae0686708f5999db4be093
403 <403@DESKTOP-5RT9B96>
2018-09-11 16:13:41 +0900

    first java programming

---
+++ src/day02/Ex_02.java
@@ -0,0 +1,16 @@
+package day02;
+
+public class Ex_02 {
+
+	public static void main(String[] args) {
+		System.out.println("JJJJJJJJJJJ       AAAAAA      VV           VV      AAAAAA");
+		System.out.println("         JJ      AA    AA      VV         VV      AA    AA");
+		System.out.println("         JJ     AAAAAAAAAA      VV       VV      AAAAAAAAAA");
+		System.out.println("         JJ     AA      AA       VV     VV       AA      AA");
+		System.out.println("         JJ     AA      AA        VV   VV        AA      AA");
+		System.out.println(" JJ     JJ      AA      AA         VV VV         AA      AA");
+		System.out.println("  JJ    JJ      AA      AA          VVV          AA      AA");
+		System.out.println("   JJJJJJ       AA      AA           V           AA      AA");
+	}
+
+}
---
+++ src/day02/Intro.java
@@ -0,0 +1,12 @@
+package day02;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		System.out.println("안녕하세요");
+		System.out.println("신이슬입니다.");
+		System.out.println("지금 매우 졸려요");
+
+	}
+
+}
